bf051b456bfd3f449dc35378acaceddc34661296
Stefan Itic <114070283+ITic5@users.noreply.github.com>
2025-02-19 11:06:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstName = "Stefan";
+let lastName = "Itic";
+
+let name = firstName + " " + lastName;
+
+let currentYear = 2025;
+let born = 1996;
+
+let age = currentYear-born;
+console.log(name + " is " + age + " years old");
+
+let setup = ["Computer", "Keyboard", "Mouse"];
+console.log(setup);
